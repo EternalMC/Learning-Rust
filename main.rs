eebd026e14fb6973055c84f52218a75d14528cd4
EternalMC <55027815+EternalMC@users.noreply.github.com>
2019-12-05 14:23:02 -0600

    Update main.rs

--- main.rs
+++ main.rs
@@ -12,4 +12,14 @@ fn run() {
     println!("{}", a);
     
     println!("{}, {}", i, a);
+    
+    let look = "Look";
+    let what = "what";
+    let i = "I";
+    let can = "can";
+    let do = "do";
+    let in = "in";
+    let rust = "rust";
+    
+    println!("{}, {}, {}, {}, {}, {}, {}", look, what, i, can, do, in, rust);
 }

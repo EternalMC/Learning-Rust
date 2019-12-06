43264cd2971ee5e647819a7362667c3a9c05224b
EternalMC <55027815+EternalMC@users.noreply.github.com>
2019-12-06 12:31:11 -0600

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,33 +1,6 @@
+mod run;
+
 fn main() {
     run();
     math();
-}
-
-fn run() {
-    println!("Rust is awesome.");
-    
-    let i = 16;
-    println!("{}", i);
-    
-    let a = 'a';
-    println!("{}", a);
-    
-    println!("{}, {}", i, a);
-    
-    let look = "Look";
-    let what = "what";
-    let i = "I";
-    let can = "can";
-    let doin = "do in";
-    let rust = "rust";
-    
-    println!("{}, {}, {}, {}, {}, {}", look, what, i, can, doin, rust);
-}
-fn math() {
-let x = 2;
-let y = 2;
-
- let z = x + y;
-
-   println!("{}", z);
 }

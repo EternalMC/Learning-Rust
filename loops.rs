f5218d64ff1ab6ca0b0ecc24069eaa7053420a79
EternalMC <55027815+EternalMC@users.noreply.github.com>
2019-12-11 11:39:33 -0600

    Update loops.rs

--- loops.rs
+++ loops.rs
@@ -1,1 +1,16 @@
+pub fn loops() {
+  println!("A simple loop");
+      for index in 0..2 {
+         println!("{}", index);
+      }
+}
+pub fn ifloop() {
+    println!("Loop in a if statement");
+let i = 9;
+    if i == 9 {
+      for index in 0..11 {
+         println!("{}", index);
+      }
+   }   
+}
 

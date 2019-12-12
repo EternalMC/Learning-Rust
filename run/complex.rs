ab81854fd691279d7a9ae5fed23ccbeef45170aa
EternalMC <55027815+EternalMC@users.noreply.github.com>
2019-12-11 21:05:14 -0600

    Update complex.rs

--- run/complex.rs
+++ run/complex.rs
@@ -32,12 +32,7 @@ use rand::Rng;
 		
 
 	      println!("Starting HP: {}", hp);
-	      try {
-			this.wait(2);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
 	      // Print random integers 
 	      if rng0 < 20  {
 	    	  println!("Dice Roll: {}", rng0);

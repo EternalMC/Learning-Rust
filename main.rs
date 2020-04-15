8bee432cfead8a55994ec693515b63273032d616
EternalMC <55027815+EternalMC@users.noreply.github.com>
2020-04-15 17:04:51 -0500

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,8 +1,12 @@
+extern crate num_cpus;
+
 mod simple;
 mod loops;
 mod run;
 // 325dfa5 is the commit
 fn main() {
+    let num = num_cpus::get();
+    println!(num);
     simple::run();
     simple::math();
     loops::loops();
